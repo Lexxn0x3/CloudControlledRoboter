9e9c3adffa14974422afbd3c73ed163cb4794071
Till Brandt <tilltb.brandt@gmail.com>
2023-11-09 11:30:41 +0100

    fixi Lidar

--- LidarData/exportLidarStream.py
+++ LidarData/exportLidarStream.py
@@ -1,26 +1,25 @@
 import socket
-import serial
+from rplidar import RPLidar
 
-# Configure serial port settings
-port = '/dev/rplidar'  # Replace with the actual port name
-baudrate = 115200
-
-# Configure TCP Conection
-ipAdress = '192.168.8.103'
-serverport = 30002
-
-# Open serial connection
-ser = serial.Serial(port, baudrate)
+# Configure TCP Connection
+ip_address = '192.168.8.103'
+server_port = 30002
 
 # Create a TCP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-sock.connect((ipAdress, serverport))
+sock.connect((ip_address, server_port))
 
-while True:
-    # Read data packet from the lidar
-    data_packet = ser.read(512)
+# Initialize RPLidar
+lidar = RPLidar('/dev/rplidar')
 
-    # Send data packet to the server
-    sock.sendall(data_packet)
+try:
+    print('Recording measurments... Press Ctrl+C to stop.')
+    for measurment in lidar.iter_measurments():
+        # Send data packet to the server
+        sock.sendall(str(measurment).encode('utf-8'))
+finally:
+    print('Stopping.')
+    lidar.stop()
+    lidar.disconnect()
