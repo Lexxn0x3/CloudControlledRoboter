bd5310871a05e36799bcc376dfdd710d0f4a7b1f
Till Brandt <tilltb.brandt@gmail.com>
2023-11-09 15:37:32 +0100

    now hopefully working. Fuck Blackbox

--- LidarData/liveDistanceData_json.py
+++ LidarData/liveDistanceData_json.py
@@ -5,29 +5,40 @@ import json
 ip_address = '192.168.8.103'
 server_port = 30002
 
-# Create a TCP socket
+# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-sock.connect((ip_address, server_port))
+# Bind the socket to the port
+server_address = (ip_address, server_port)
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
 
 while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+
     try:
-        # Receive distance measurement data from the sender
-        data = sock.recv(1024)
-        if not data:
-            break
+        print('connection from', client_address)
 
-        # Decode the JSON data and extract the distance and angle
-        data = json.loads(data.decode('utf-8'))
-        angle = data['angle']
-        distance = data['distance']
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(1024)
+            if data:
+                # Decode the JSON data and extract the distance and angle
+                data = json.loads(data.decode('utf-8'))
+                angle = data['angle']
+                distance = data['distance']
 
-        # Print the distance measurement with the corresponding angle
-        print(f'Angle: {angle}, Distance: {distance}')
-
+                # Print the distance measurement with the corresponding angle
+                print(f'Angle: {angle}, Distance: {distance}')
+            else:
+                break
+            
     except KeyboardInterrupt:
         break
-
-# Close the TCP socket
-sock.close()
+    finally:
+        # Clean up the connection
+        connection.close()
