e444ef3bd8bc0a7df3d1ba5c8dd02ae7546c155a
Lexx <35461915+Lexxn0x3@users.noreply.github.com>
2023-11-21 15:12:46 +0100

    TCP Broker from Broker Branch

---
+++ TcpBroker/Broker/src/config.rs
@@ -0,0 +1,58 @@
+use clap::{App, Arg};
+
+pub struct Config {
+    pub server_port: u16,
+    pub client_port: u16,
+    pub debug_level: String,
+    pub buffer_size: usize,
+}
+
+pub fn parse_arguments() -> Config {
+    let matches = App::new("My Program")
+        .arg(Arg::with_name("server-port")
+            .long("server-port")
+            .help("Sets the server port")
+            .takes_value(true)
+            .default_value("3001")
+            .validator(validate_port))
+        .arg(Arg::with_name("client-port")
+            .long("client-port")
+            .help("Sets the client port")
+            .takes_value(true)
+            .default_value("4001")
+            .validator(validate_port))
+        .arg(Arg::with_name("debug-level")
+            .long("debug-level")
+            .help("Sets the debug level")
+            .takes_value(true)
+            .default_value("info")
+            .possible_values(&["trace", "debug", "info", "warn", "error"]))
+        .arg(Arg::with_name("buffer-size")
+            .long("buffer-size")
+            .help("Sets the buffer size in bytes")
+            .takes_value(true)
+            .default_value("131072")
+            .validator(|v| v.parse::<usize>()
+                             .map(|_| ())
+                             .map_err(|_| String::from("Buffer size must be an integer"))))
+        .get_matches();
+
+    Config {
+        server_port: matches.value_of("server-port").unwrap().parse().unwrap(),
+        client_port: matches.value_of("client-port").unwrap().parse().unwrap(),
+        debug_level: matches.value_of("debug-level").unwrap().to_string(),
+        buffer_size: matches.value_of("buffer-size").unwrap().parse().unwrap(),
+    }
+}
+
+fn validate_port(v: &str) -> Result<(), String> {
+    v.parse::<u16>()
+        .map_err(|_| String::from("The port must be a number"))
+        .and_then(|val| {
+            if val > 1024 && val < 65535 {
+                Ok(())
+            } else {
+                Err(String::from("Port must be a number between 1025 and 65534"))
+            }
+        })
+}
--- TcpBroker/Broker/src/main.rs
+++ TcpBroker/Broker/src/main.rs
@@ -1,82 +1,81 @@
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::sync::{mpsc, Mutex};
-use tokio::time::{self, Duration, Instant};
-use std::sync::Arc;
 use std::collections::HashMap;
-use std::net::SocketAddr;
 use std::io::{self, Write};
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Instant;
 
-struct TrafficStats {
-    bytes_received: usize,
-    bytes_sent: HashMap<SocketAddr, usize>,
-    last_print_time: Instant,
-}
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    execute,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{mpsc, Mutex};
+use tokio::time::{self, Duration};
+use tui::backend::CrosstermBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Block, Borders, Gauge, List, ListItem, Paragraph};
+use tui::Terminal;
 
-impl TrafficStats {
-    fn new() -> Self {
-        Self {
-            bytes_received: 0,
-            bytes_sent: HashMap::new(),
-            last_print_time: Instant::now(),
-        }
-    }
-
-    fn add_received(&mut self, bytes: usize) {
-        self.bytes_received += bytes;
-    }
-
-    fn add_sent(&mut self, addr: SocketAddr, bytes: usize) {
-        *self.bytes_sent.entry(addr).or_insert(0) += bytes;
-    }
-
-    fn print_and_reset(&mut self) {
-        let time_elapsed = self.last_print_time.elapsed().as_secs_f64();
-        let received_speed = self.bytes_received as f64 / time_elapsed;
-        
-        println!("Received: {} bytes ({} bytes/sec)", self.bytes_received, received_speed);
-        for (addr, &bytes) in &self.bytes_sent {
-            let sent_speed = bytes as f64 / time_elapsed;
-            println!("Sent to {}: {} bytes ({} bytes/sec)", addr, bytes, sent_speed);
-        }
-        
-        // Reset statistics
-        self.bytes_received = 0;
-        self.bytes_sent.clear();
-        self.last_print_time = Instant::now();
-    }
-}
+mod config;
+mod ui;
+use ui::UI;
+mod statistics;
+use statistics::Statistics;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let config = config::parse_arguments();
+    let stats = Arc::new(Mutex::new(Statistics::new()));
+
+    // Enter the alternate screen and clear it
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, terminal::Clear(terminal::ClearType::All))?;
+
+    // Wrap the UI in an Arc<Mutex<>> to share between contexts
+    let ui = Arc::new(Mutex::new(UI::new()?));
+
     let (tx, mut rx) = mpsc::channel::<Vec<u8>>(100);
-    let stats = Arc::new(Mutex::new(TrafficStats::new()));
 
     // Periodically print statistics
-    let stats_for_printing = Arc::clone(&stats);
+    let stats_for_ui = Arc::clone(&stats);
+    let ui_for_drawing = Arc::clone(&ui);
     tokio::spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(5));
+        let mut interval = time::interval(Duration::from_millis(100));
         loop {
             interval.tick().await;
-            print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear the console
-            let mut stats = stats_for_printing.lock().await;
-            stats.print_and_reset();
-            io::stdout().flush().unwrap();
+            let mut ui = ui_for_drawing.lock().await;
+            let mut stats = stats_for_ui.lock().await;
+            
+            let (received_throughput, sent_throughput) = stats.throughput(); // Get current throughput
+            
+            ui.data_throughput = received_throughput;
+            ui.buffer_size = config.buffer_size;
+            ui.buffer_usage = stats.buffer_usage;
+
+            if let Err(e) = ui.draw()
+            {
+                eprintln!("Error drawing UI: {}", e);
+            }
         }
     });
 
     // Accept a single connection for receiving data
     let stats_for_reading = Arc::clone(&stats);
     let read_handle = tokio::spawn(async move {
-        let listener = TcpListener::bind("0.0.0.0:12345").await.unwrap();
+        let listener = TcpListener::bind(format!("0.0.0.0:{}", config.server_port)).await.unwrap();
+        println!("Server is running for receiving data on port: {}", config.server_port); // Print server start information
         if let Ok((mut socket, _)) = listener.accept().await {
-            let mut buf = [0u8; 4096];
+            let mut buf = vec![0u8; config.buffer_size]; // Create a buffer with the configured size
             loop {
                 match socket.read(&mut buf).await {
                     Ok(0) => break, // Connection was closed
                     Ok(n) => {
                         let mut stats = stats_for_reading.lock().await;
                         stats.add_received(n);
+                        stats.set_buffer_usage(n);
                         tx.send(buf[..n].to_vec()).await.unwrap();
                     }
                     Err(e) => eprintln!("Failed to read from socket: {:?}", e),
@@ -88,7 +87,8 @@ async fn main() -> std::io::Result<()> {
     // Accept multiple connections for sending data
     let stats_for_writing = Arc::clone(&stats);
     let write_handle = tokio::spawn(async move {
-        let listener = TcpListener::bind("0.0.0.0:54321").await.unwrap();
+        let listener = TcpListener::bind(format!("0.0.0.0:{}", config.client_port)).await.unwrap();
+        println!("Server is running for sending data on port: {}", config.client_port); // Print server start information
         let client_map = Arc::new(Mutex::new(HashMap::<SocketAddr, TcpStream>::new()));
 
         // Accept new clients and store them in a HashMap
@@ -112,7 +112,6 @@ async fn main() -> std::io::Result<()> {
                     disconnected.push(*addr); // Mark the client for removal
                 } else {
                     let mut stats = stats_for_writing.lock().await;
-                    stats.add_sent(*addr, data.len());
                 }
             }
 
@@ -125,6 +124,14 @@ async fn main() -> std::io::Result<()> {
 
     // Wait for both tasks to complete
     let _ = tokio::try_join!(read_handle, write_handle);
+
+    {
+        let mut ui = ui.lock().await;
+        ui.cleanup()?;
+
+        // Leave the alternate screen
+        execute!(io::stdout(), LeaveAlternateScreen)?;
+    }
 
     Ok(())
 }
