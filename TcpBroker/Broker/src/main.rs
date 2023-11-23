5b6f6e4102b625c50d7a85607366b18226f41e8f
Lexx <35461915+Lexxn0x3@users.noreply.github.com>
2023-11-23 18:38:07 +0100

    Rewrite TCP Broker
    -every tcp connection on seperate thread
    -loggin level info, debug, error
    -rename of server and client to single and multi connection
    -freezing fixed on clinet error
    -broker now sending all data regardless, auto removing client if not accepting data
    - support dis and reconection of single and multi conection clients

--- TcpBroker/Broker/src/main.rs
+++ TcpBroker/Broker/src/main.rs
@@ -1,135 +1,122 @@
-use std::collections::HashMap;
-use std::io;
-use std::net::SocketAddr;
-use std::sync::Arc;
-use crossterm::{
-    execute,
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{mpsc, Mutex};
-use tokio::time::{self, Duration};
-
 mod config;
-mod ui;
-use ui::UI;
-mod statistics;
-use statistics::Statistics;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write, Error};
+use std::sync::mpsc::{Sender};
+use env_logger;
+use log::{info, error, debug};
+use env_logger::{Builder, Env};
+
+fn main()
+{
+    //Get config from Args
     let config = config::parse_arguments();
-    let stats = Arc::new(Mutex::new(Statistics::new()));
 
-    // Enter the alternate screen and clear it
-    if config.debug_level != "none"
-    {
-        let mut stdout = io::stdout();
-        execute!(stdout, EnterAlternateScreen, terminal::Clear(terminal::ClearType::All))?;
+    // Initialize the logger with the specified level
+    Builder::from_env(Env::default().default_filter_or(config.debug_level)).init();
+
+
+    let client_senders = Arc::new(Mutex::new(vec![]));
+
+    // Start single_connection_listener in a separate thread
+    let client_senders_clone = Arc::clone(&client_senders);
+    thread::spawn(move || {
+        single_connection_listener(client_senders_clone, config.single_connection_port, config.buffer_size);
+    });
+
+    multi_conection_listener(&client_senders, config.multi_connection_port);
+}
+
+fn multi_conection_listener(client_senders: &Arc<Mutex<Vec<Sender<Vec<u8>>>>>, port: u16) {
+    let multi_listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+    info!("Multi-connection TCP listener started on {}:{}", multi_listener.local_addr().unwrap().ip(), multi_listener.local_addr().unwrap().port());
+
+    for stream in multi_listener.incoming() {
+        let client_senders_clone = Arc::clone(&client_senders);
+        if let Ok(stream) = stream {
+            debug!("Accepted new multi connection from {}:{}", stream.peer_addr().unwrap().ip(), stream.peer_addr().unwrap().port());
+
+            let (client_tx, client_rx) = mpsc::channel();
+            client_senders_clone.lock().unwrap().push(client_tx);
+
+            thread::spawn(move || {
+                handle_client_connection(stream, client_rx).unwrap();
+            });
+        }
     }
+}
 
-    // Wrap the UI in an Arc<Mutex<>> to share between contexts
-    let ui = Arc::new(Mutex::new(UI::new()?));
+fn single_connection_listener(client_senders: Arc<Mutex<Vec<Sender<Vec<u8>>>>>, port: u16, buffer_size: usize) {
+    let single_listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+    info!("Single-connection TCP listener started on {}:{}", single_listener.local_addr().unwrap().ip(), single_listener.local_addr().unwrap().port());
 
-    let (tx, mut rx) = mpsc::channel::<Vec<u8>>(100);
-
-
-    if config.debug_level != "none"
-    {
-        // Periodically print statistics
-        let stats_for_ui = Arc::clone(&stats);
-        let ui_for_drawing = Arc::clone(&ui);
-        tokio::spawn(async move {
-            let mut interval = time::interval(Duration::from_millis(100));
-            loop {
-                interval.tick().await;
-                let mut ui = ui_for_drawing.lock().await;
-                let mut stats = stats_for_ui.lock().await;
-
-                let (received_throughput,_sent_throughput) = stats.throughput(); // Get current throughput
-
-                ui.data_throughput = received_throughput;
-                ui.buffer_size = config.buffer_size;
-                ui.buffer_usage = stats.buffer_usage;
-
-                if let Err(e) = ui.draw()
-                {
-                    eprintln!("Error drawing UI: {}", e);
+    loop {
+        let client_senders_clone = Arc::clone(&client_senders);
+        match single_listener.accept() {
+            Ok((stream, _)) => {
+                debug!("Accepted single connection from {}:{}", stream.peer_addr().unwrap().ip(), stream.peer_addr().unwrap().port());
+                if let Err(e) = handle_single_connection(stream, client_senders_clone, buffer_size) {
+                    error!("Error handling single connection: {}", e);
+                    // Here you can handle any cleanup or reset actions needed
                 }
-            }
-        });
+            },
+            Err(e) => error!("Failed to accept connection: {}", e),
+        }
     }
+}
 
-    // Accept a single connection for receiving data
-    let stats_for_reading = Arc::clone(&stats);
-    let read_handle = tokio::spawn(async move {
-        let listener = TcpListener::bind(format!("0.0.0.0:{}", config.server_port)).await.unwrap();
-        println!("Server is running for receiving data on port: {}", config.server_port); // Print server start information
-        if let Ok((mut socket, _)) = listener.accept().await {
-            let mut buf = vec![0u8; config.buffer_size]; // Create a buffer with the configured size
-            loop {
-                match socket.read(&mut buf).await {
-                    Ok(0) => break, // Connection was closed
-                    Ok(n) => {
-                        let mut stats = stats_for_reading.lock().await;
-                        stats.add_received(n);
-                        stats.set_buffer_usage(n);
-                        tx.send(buf[..n].to_vec()).await.unwrap();
+fn handle_single_connection(mut stream: TcpStream, clients: Arc<Mutex<Vec<mpsc::Sender<Vec<u8>>>>>, buffer_size: usize) -> Result<(), Error> {
+    debug!("Single connection handler started.");
+    let mut buffer = vec![0; buffer_size];  // Dynamic buffer based on buffer_size
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(nbytes) => {
+                if nbytes == 0 {
+                    debug!("Single connection closed by the client.");
+                    break;
+                }
+                let data = buffer[..nbytes].to_vec();
+                let mut clients_to_remove = vec![];
+                for (i, client) in clients.lock().unwrap().iter().enumerate() {
+                    if client.send(data.clone()).is_err() {
+                        error!("Error sending data to a multi-connection client. Removing client.");
+                        clients_to_remove.push(i);
                     }
-                    Err(e) => eprintln!("Failed to read from socket: {:?}", e),
                 }
+                let mut clients = clients.lock().unwrap();
+                for i in clients_to_remove.iter().rev() {
+                    clients.remove(*i);
+                }
+            },
+            Err(e) => {
+                error!("Error reading from single connection: {}", e);
+                break;
             }
         }
-    });
-
-    // Accept multiple connections for sending data
-    //let stats_for_writing = Arc::clone(&stats);
-    let write_handle = tokio::spawn(async move {
-        let listener = TcpListener::bind(format!("0.0.0.0:{}", config.client_port)).await.unwrap();
-        println!("Server is running for sending data on port: {}", config.client_port); // Print server start information
-        let client_map = Arc::new(Mutex::new(HashMap::<SocketAddr, TcpStream>::new()));
-
-        // Accept new clients and store them in a HashMap
-        let client_map_clone = Arc::clone(&client_map);
-        tokio::spawn(async move {
-            loop {
-                if let Ok((socket, addr)) = listener.accept().await {
-                    client_map_clone.lock().await.insert(addr, socket);
-                }
-            }
-        });
-
-        // Read from the channel and write to all connected clients
-        while let Some(data) = rx.recv().await {
-            let mut clients = client_map.lock().await;
-            let mut disconnected = Vec::new(); // Track disconnected clients
-
-            for (addr, socket) in clients.iter_mut() {
-                if let Err(e) = socket.write_all(&data).await {
-                    eprintln!("Failed to write to client {}: {}", addr, e);
-                    disconnected.push(*addr); // Mark the client for removal
-                } else
-                {
-                    //let mut stats = stats_for_writing.lock().await;
-                }
-            }
-
-            // Remove disconnected clients
-            for addr in disconnected {
-                clients.remove(&addr);
-            }
-        }
-    });
-
-    // Wait for both tasks to complete
-    let _ = tokio::try_join!(read_handle, write_handle);
-    {
-        let mut ui = ui.lock().await;
-        ui.cleanup()?;
-
-        // Leave the alternate screen
-        execute!(io::stdout(), LeaveAlternateScreen)?;
     }
+    debug!("Single connection handler ended.");
+    Ok(())
+}
+
+fn handle_client_connection(mut stream: TcpStream, rx: mpsc::Receiver<Vec<u8>>) -> Result<(), Error> {
+    debug!("Multi-connection client handler started.");
+    loop {
+        match rx.recv() {
+            Ok(data) => {
+                if stream.write_all(&data).is_err() {
+                    error!("Error writing to multi-connection client. Client may have disconnected.");
+                    break;
+                }
+            },
+            Err(_) => {
+                debug!("Multi-connection client disconnected. Ending handler thread.");
+                break;
+            }
+        }
+    }
+    debug!("Multi-connection client handler ended.");
     Ok(())
 }
