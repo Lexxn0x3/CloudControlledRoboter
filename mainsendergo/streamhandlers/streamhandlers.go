fa4821d9488c6dd389e315ce3d824989a11dc2a3
Mattis Krämer <mattis.kraemer@fieldforce.de>
2023-12-07 14:39:39 +0100

    feat reconnects

--- mainsendergo/streamhandlers/streamhandlers.go
+++ mainsendergo/streamhandlers/streamhandlers.go
@@ -15,55 +15,83 @@ import (
 func HandleCameraStream(addr string, port string, doneChan chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	fmt.Printf("Starting ffmpeg on port %s\n", port)
-	cmd := exec.Command("ffmpeg", "-input_format", "mjpeg", "-i", "/dev/video0", "-c:v", "copy", "-f", "mjpeg", fmt.Sprintf("tcp://%s:%s", addr, port))
+	for {
+		fmt.Printf("Starting ffmpeg on port %s\n", port)
+		cmd := exec.Command("ffmpeg", "-input_format", "mjpeg", "-i", "/dev/video0", "-c:v", "copy", "-f", "mjpeg", fmt.Sprintf("tcp://%s:%s", addr, port))
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("Error starting camera stream on port %s: %v\n", port, err)
-		return
-	}
-
-	go func() {
-		err := cmd.Wait()
-		if err != nil {
-			fmt.Printf("ffmpeg exited with error on port %s: %v\n", port, err)
-		} else {
-			fmt.Printf("ffmpeg exited normally on port %s\n", port)
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("Error starting camera stream on port %s: %v\n", port, err)
+			return
 		}
-		if _, ok := <-doneChan; ok {
-			close(doneChan)
-		}
-	}()
 
-	<-doneChan
-	if cmd.Process != nil {
-		fmt.Printf("Stopping ffmpeg on port %s\n", port)
-		cmd.Process.Kill()
+		errChan := make(chan error)
+		go func() {
+			errChan <- cmd.Wait()
+		}()
+
+		select {
+		case err := <-errChan:
+			if err != nil {
+				fmt.Printf("ffmpeg exited with error on port %s: %v\n", port, err)
+				fmt.Println("Attempting to restart ffmpeg...")
+				continue
+			} else {
+				fmt.Printf("ffmpeg exited normally on port %s\n", port)
+				return
+			}
+		case <-doneChan:
+			if _, ok := <-doneChan; ok {
+				close(doneChan)
+			}
+			if cmd.Process != nil {
+				fmt.Printf("Stopping ffmpeg on port %s\n", port)
+				cmd.Process.Kill()
+			}
+			return
+		}
 	}
 }
 
 func HandleLidarStream(addr string, port string, doneChan chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%s", addr, port))
-	if err != nil {
-		fmt.Println("Error resolving TCP address:", err)
-		return
-	}
+	for {
+		select {
+		case <-doneChan:
+			return
+		default:
+			// Attempt to establish TCP connection
+			tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%s", addr, port))
+			if err != nil {
+				fmt.Println("Error resolving TCP address:", err)
+				time.Sleep(time.Second * 2) // Wait before retrying
+				continue
+			}
 
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		fmt.Printf("Error connecting to LiDAR server at %s:%s: %v\n", addr, port, err)
-		return
+			conn, err := net.DialTCP("tcp", nil, tcpAddr)
+			if err != nil {
+				fmt.Printf("Error connecting to LiDAR server at %s:%s: %v\n", addr, port, err)
+				time.Sleep(time.Second * 2) // Wait before retrying
+				continue
+			}
+
+			// If connected, handle the connection
+			handleConnection(conn, doneChan)
+
+			conn.Close()                // Close the connection when handleConnection returns
+			time.Sleep(time.Second * 2) // Wait before trying to reconnect
+		}
 	}
-	defer conn.Close()
+}
+
+func handleConnection(conn *net.TCPConn, doneChan chan struct{}) {
 	conn.SetNoDelay(true)
 
 	lidar := rplidar.NewRPLidar("/dev/rplidar", 115200, time.Second*3)
-	err = lidar.Connect()
+	err := lidar.Connect()
 	if err != nil {
 		fmt.Println("Error connecting to RPLidar:", err)
 		return
@@ -107,19 +135,37 @@ func HandleLidarStream(addr string, port string, doneChan chan struct{}, wg *syn
 func HandleBatteryStream(addr string, port string, doneChan chan struct{}, wg *sync.WaitGroup, rm *rosmasterlib.Rosmaster) {
 	defer wg.Done()
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%s", addr, port))
-	if err != nil {
-		fmt.Println("Error resolving TCP address:", err)
-		return
-	}
+	for {
+		select {
+		case <-doneChan:
+			return // Exit if a done signal is received
+		default:
+			// Attempt to establish TCP connection
+			tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%s", addr, port))
+			if err != nil {
+				fmt.Println("Error resolving TCP address:", err)
+				time.Sleep(time.Second * 2) // Wait before retrying
+				continue
+			}
 
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		fmt.Printf("Error connecting to Battery server at %s:%s: %v\n", addr, port, err)
-		return
+			conn, err := net.DialTCP("tcp", nil, tcpAddr)
+			if err != nil {
+				fmt.Printf("Error connecting to Battery server at %s:%s: %v\n", addr, port, err)
+				time.Sleep(time.Second * 2) // Wait before retrying
+				continue
+			}
+			conn.SetNoDelay(true)
+
+			// If connected, handle the connection
+			handleBatteryConnection(conn, doneChan, rm)
+
+			conn.Close()                // Close the connection when handleBatteryConnection returns
+			time.Sleep(time.Second * 2) // Wait before trying to reconnect
+		}
 	}
-	conn.SetNoDelay(true)
-	defer conn.Close()
+}
+
+func handleBatteryConnection(conn *net.TCPConn, doneChan chan struct{}, rm *rosmasterlib.Rosmaster) {
 	for {
 		select {
 		case <-time.After(1 * time.Second): // Timer for sending battery voltage
