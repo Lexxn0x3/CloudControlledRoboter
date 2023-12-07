7812fff00b4e0286c9f943ce2639da4bff7fec35
Lexx <35461915+Lexxn0x3@users.noreply.github.com>
2023-12-07 10:44:03 +0100

    desable websocket option

--- TcpBroker/Broker/src/config.rs
+++ TcpBroker/Broker/src/config.rs
@@ -4,6 +4,7 @@ pub struct Config {
     pub single_connection_port: u16,
     pub multi_connection_port: u16,
     pub websocket_connection_port: u16,
+    pub no_websocket: bool,
     pub debug_level: String,
     pub buffer_size: usize,
 }
@@ -12,36 +13,46 @@ pub fn parse_arguments() -> Config {
     let matches = App::new("My Program")
         .arg(Arg::with_name("single-connection-port")
             .long("single-connection-port")
-            .help("Sets the single connection port")
+            .short('s')  // Changed to a char
+            .help("Sets the port for single connections. Default is 3001.")
             .takes_value(true)
             .default_value("3001")
             .validator(validate_port))
         .arg(Arg::with_name("multi-connection-port")
             .long("multi-connection-port")
-            .help("Sets the multi connection port")
+            .short('m')  // Changed to a char
+            .help("Sets the port for multiple connections. Default is 9001.")
             .takes_value(true)
             .default_value("9001")
             .validator(validate_port))
         .arg(Arg::with_name("websocket-connection-port")
             .long("websocket-connection-port")
-            .help("Sets the websocket connection port")
+            .short('w')  // Changed to a char
+            .help("Sets the port for websocket connections. Default is 5001.")
             .takes_value(true)
             .default_value("5001")
             .validator(validate_port))
+        .arg(Arg::with_name("no-websocket")
+            .long("no-websocket")
+            .short('n')  // Changed to a char
+            .help("Disables the websocket connection functionality.")
+            .takes_value(false))
         .arg(Arg::with_name("debug-level")
             .long("debug-level")
-            .help("Sets the debug level")
+            .short('d')  // Changed to a char
+            .help("Sets the logging debug level. Possible values are 'info', 'error', 'debug'. Default is 'info'.")
             .takes_value(true)
             .default_value("info")
             .possible_values(&["info", "error", "debug"]))
         .arg(Arg::with_name("buffer-size")
             .long("buffer-size")
-            .help("Sets the buffer size in bytes: higher values would be 131072 or 512000 (standard)")
+            .short('b')  // Changed to a char
+            .help("Sets the buffer size in bytes. Higher values might be 131072 or 512000 (standard). Default is 512000.")
             .takes_value(true)
             .default_value("512000")
             .validator(|v| v.parse::<usize>()
-                             .map(|_| ())
-                             .map_err(|_| String::from("Buffer size must be an integer"))))
+                .map(|_| ())
+                .map_err(|_| String::from("Buffer size must be an integer"))))
         .get_matches();
 
     Config
@@ -49,6 +60,7 @@ pub fn parse_arguments() -> Config {
         single_connection_port: matches.value_of("single-connection-port").unwrap().parse().unwrap(),
         multi_connection_port: matches.value_of("multi-connection-port").unwrap().parse().unwrap(),
         websocket_connection_port: matches.value_of("websocket-connection-port").unwrap().parse().unwrap(),
+        no_websocket: matches.is_present("no-websocket"),
         debug_level: matches.value_of("debug-level").unwrap().to_string(),
         buffer_size: matches.value_of("buffer-size").unwrap().parse().unwrap(),
     }
@@ -58,7 +70,8 @@ fn validate_port(v: &str) -> Result<(), String> {
     v.parse::<u16>()
         .map_err(|_| String::from("The port must be a number"))
         .and_then(|val| {
-            if val > 1024 && val < 65535 {
+            if val > 1024 && val < 65535
+            {
                 Ok(())
             } else {
                 Err(String::from("Port must be a number between 1025 and 65534"))
